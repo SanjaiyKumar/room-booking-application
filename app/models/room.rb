e71679fad000d29230699a50a993a7e0e31ebc77
SanjaiyKumar <sanjaiykumar.tn@rently.com>
2021-06-15 18:04:58 +0530

    added Room Validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
     belongs_to :hotel
+    validates :price, presence: true
+    validates :room_no , presence: true
+    validates :room_no , uniqueness: { scope: :hotel_id , message: " : %{value}  already exists" }
+    validates :no_of_beds , presence: {message: " can't be empty"}
+    validates :room_type , presence: true
 end
