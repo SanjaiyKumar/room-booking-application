356312f1e6e354ae9aa7d41e9a7256c1d5335be0
SanjaiyKumar <sanjaiykumar.tn@rently.com>
2021-06-16 17:45:03 +0530

    Added User view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/index' ,:to => 'user#index' , as: :user_hotel_index
+  get 'user/:id/show' ,:to => 'user#show' , as: :user_hotel_show
+  get 'user/:id/showroom' ,:to => 'user#showroom' , as: :user_hotel_showroom
+  get 'user/:id/bookingpage' ,:to => 'user#bookingpage' , as: :user_bookingpage
   devise_for :hotels
   get '/rooms/:id/changestatus/' , :to => 'rooms#changestatus' , as: :change_status 
   resources :rooms
